ceb999b6efcb7a5023438e7c14ed1bfa783b9266
Margarita Muhina <ritamuhina591@gmail.com>
2025-03-16 15:33:28 +0300

    v1

--- src/TimedDoor.cpp
+++ src/TimedDoor.cpp
@@ -1,2 +1,45 @@
 // Copyright 2021 GHA Test Team
-#include "TimedDoor.h"
+#include "../include/TimedDoor.h"
+#include <thread>
+#include <chrono>
+#include <stdexcept>
+
+DoorTimerAdapter::DoorTimerAdapter(TimedDoor& door) : door(door) {}
+
+void DoorTimerAdapter::Timeout() {
+  door.throwState();
+}
+
+TimedDoor::TimedDoor(int timeout) : iTimeout(timeout), isOpened(false),
+     adapter(new DoorTimerAdapter(*this)) {}
+
+bool TimedDoor::isDoorOpened() {
+  return isOpened;
+}
+
+void TimedDoor::unlock() {
+  isOpened = true;
+}
+
+void TimedDoor::lock() {
+  isOpened = false;
+}
+
+int TimedDoor::getTimeOut() const {
+  return iTimeout;
+}
+
+void TimedDoor::throwState() {
+  if (isOpened) {
+    throw std::runtime_error("Door is still open after timeout!");
+  }
+}
+
+void Timer::tregister(int timeout, TimerClient* client) {
+  std::this_thread::sleep_for(std::chrono::milliseconds(timeout));
+  client->Timeout();
+}
+
+TimedDoor::~TimedDoor() {
+  delete adapter;
+}
--- test/tests.cpp
+++ test/tests.cpp
@@ -2,5 +2,116 @@
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-#include <cstdint>
-#include "TimedDoor.h"
+#include <chrono>
+#include <thread>
+#include "../include/TimedDoor.h"
+
+using ::testing::_;
+using ::testing::AtLeast;
+using ::testing::Invoke;
+
+class MockTimerClient : public TimerClient {
+public:
+    MOCK_METHOD(void, Timeout, (), (override));
+};
+
+class MockTimer : public Timer {
+public:
+    MOCK_METHOD(void, tregister, (int, TimerClient*), (override));
+};
+
+class TimedDoorTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        timedDoor = new TimedDoor(100);
+    }
+
+    void TearDown() override {
+        delete timedDoor;
+    }
+
+    TimedDoor* timedDoor;
+};
+
+TEST_F(TimedDoorTest, DoorInitialStateIsClosed) {
+    ASSERT_FALSE(timedDoor->isDoorOpened());
+}
+
+TEST_F(TimedDoorTest, UnlockOpensTheDoor) {
+    timedDoor->unlock();
+    ASSERT_TRUE(timedDoor->isDoorOpened());
+}
+
+TEST_F(TimedDoorTest, LockClosesTheDoor) {
+    timedDoor->unlock();
+    timedDoor->lock();
+    ASSERT_FALSE(timedDoor->isDoorOpened());
+}
+
+TEST_F(TimedDoorTest, GetTimeout) {
+    ASSERT_EQ(timedDoor->getTimeOut(), 100);
+}
+
+TEST_F(TimedDoorTest, ThrowsExceptionWhenDoorIsOpen) {
+    timedDoor->unlock();
+    ASSERT_THROW(timedDoor->adapter->Timeout(), std::runtime_error);
+}
+
+TEST_F(TimedDoorTest, DoesNotThrowExceptionWhenDoorIsClosed) {
+    timedDoor->lock();
+    ASSERT_NO_THROW(timedDoor->adapter->Timeout());
+}
+
+TEST(DoorTimerAdapterTest, TimeoutCallsThrowStateOnDoor) {
+    class MockTimedDoor : public TimedDoor {
+     public:
+      MockTimedDoor(int timeout) : TimedDoor(timeout) {}
+      MOCK_METHOD(void, throwState, (), (override));
+    };
+
+    MockTimedDoor mockDoor(10);
+    DoorTimerAdapter adapter(mockDoor);
+    EXPECT_CALL(mockDoor, throwState())
+        .Times(1);
+    adapter.Timeout();
+}
+
+
+TEST(TimerTest, tregisterCallsTimeoutWithMockTimer) {
+  MockTimerClient mockClient;
+  MockTimer mockTimer;
+
+  EXPECT_CALL(mockTimer, tregister(50, _)) 
+      .Times(1);
+  EXPECT_CALL(mockClient, Timeout())
+      .Times(1);
+  mockTimer.tregister(50, &mockClient); 
+}
+
+TEST(TimedDoorIntegrationTest, OpenDoorThrowsExceptionAfterTimeoutWithMockTimer) {
+  TimedDoor door(50);
+  MockTimer mockTimer;
+  door.unlock();
+  EXPECT_CALL(mockTimer, tregister(door.getTimeOut(), door.adapter))
+      .Times(1)
+      .WillOnce(Invoke([&door]() { 
+          door.adapter->Timeout(); 
+      }));
+  mockTimer.tregister(door.getTimeOut(), door.adapter); 
+  ASSERT_THROW(door.throwState(), std::runtime_error);
+  delete door.adapter;
+}
+
+TEST(TimedDoorIntegrationTest, ClosedDoorDoesNotThrowExceptionAfterTimeoutWithMockTimer) {
+  TimedDoor door(50);
+  MockTimer mockTimer;
+  door.lock();
+    EXPECT_CALL(mockTimer, tregister(door.getTimeOut(), door.adapter))
+        .Times(1)
+        .WillOnce(Invoke([&door]() { 
+            door.adapter->Timeout(); 
+        }));
+  mockTimer.tregister(door.getTimeOut(), door.adapter);
+  ASSERT_NO_THROW(door.throwState());
+  delete door.adapter;
+}
