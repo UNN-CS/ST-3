90ac1b81597c857884f018a1fcef99e8f5e0c4cd
shyravina19102004 <shyravina19102004@gmail.com>
2025-03-17 19:12:03 +0300

    20

--- src/TimedDoor.cpp
+++ src/TimedDoor.cpp
@@ -53,7 +53,6 @@ int TimedDoor::getTimeOut() const {
   return iTimeout;
 }
 
-// Реализация Timer
 void Timer::sleep(int milliseconds) {
   if (milliseconds > 0) {
     std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
