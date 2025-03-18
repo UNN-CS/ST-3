e6ef2a92d4c4a06f3802753ba5a4688cf9c43351
Ivan <sadikov.04@list.ru>
2025-03-18 21:48:30 +0300

    test fix

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,15 +1,39 @@
 // Copyright 2021 GHA Test Team
 
+#include <cstdint>
+#include <iostream>
+#include <stdexcept>
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-#include <cstdint>
-
 #include "TimedDoor.h"
 
-TEST(timed_door_tests, test_num_1) {
-  TimedDoor door(5);
-  door.unlock();
-  door.unlock();
-  EXPECT_TRUE(door.isDoorOpened(), false);
+class MockDoorTimerAdapter : public TimerClient
+{
+public:
+  MOCK_METHOD(void, Timeout, (), (override));
+};
+
+class MockDoor : public Door
+{
+public:
+  MOCK_METHOD(void, lock, (), (override));
+  MOCK_METHOD(void, unlock, (), (override));
+  MOCK_METHOD(bool, isDoorOpened, (), (const, override));
+};
+
+class TimeDoorTests : public ::testing::Test
+{
+public:
+  void SetUp() override { m_door = std::make_unique<TimedDoor>(); }
+
+  void TearDown() override { m_door.reset(); }
+
+  std::unique_ptr<TimedDoor> m_door;
+};
+
+TEST(TimeDoorTests, test_num_1)
+{
+  EXPECT_FALSE(m_door->isDoorOpened());
 }
