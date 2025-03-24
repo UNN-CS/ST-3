78bee2e458f4fe6d8b2ee5261da46b05ce20fd14
ermilovad <112872618+ermilovad@users.noreply.github.com>
2025-03-24 20:43:16 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -10,19 +10,19 @@ using ::testing::Mock;
 using ::testing::_;
 
 class MockTimerClient : public TimerClient {
-public:
+ public:
     MOCK_METHOD(void, Timeout, (), (override));
 };
 
 class MockDoor : public Door {
-public:
+ public:
     MOCK_METHOD(void, lock, (), (override));
     MOCK_METHOD(void, unlock, (), (override));
     MOCK_METHOD(bool, isDoorOpened, (), (override));
 };
 
 class TimedDoorTest : public ::testing::Test {
-protected:
+ protected:
     TimedDoor* door;
     DoorTimerAdapter* adapter;
     Timer* timer;
@@ -87,7 +87,7 @@ TEST(TimedDoorTest, timer_calls_timeout_method) {
     EXPECT_CALL(mockClient, Timeout()).Times(1);
 
     timer.tregister(1, &mockClient);
-    std::this_thread::sleep_for(std::chrono::seconds(2)); 
+    std::this_thread::sleep_for(std::chrono::seconds(2));
 }
 
 TEST_F(TimedDoorTest, locking_twice_door_lock) {
