f41fa7b41b461bb5d5269ea7663daeae22e6b85b
ermilovad <112872618+ermilovad@users.noreply.github.com>
2025-03-24 20:42:20 +0300

    Update TimedDoor.cpp

--- src/TimedDoor.cpp
+++ src/TimedDoor.cpp
@@ -13,11 +13,10 @@ void DoorTimerAdapter::Timeout() {
 }
 
 TimedDoor::TimedDoor(int timeout) : iTimeout(timeout), isOpened(false) {
-    adapter = new DoorTimerAdapter(*this); 
+    adapter = new DoorTimerAdapter(*this);
 }
 
-
-bool TimedDoor::isDoorOpened(){
+bool TimedDoor::isDoorOpened() {
     return isOpened;
 }
 
