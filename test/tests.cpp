9aa99a4b45954828da5210ddfa8c17c7037d9a99
Kirill Khasanyanov <112872816+khasanyanovk@users.noreply.github.com>
2025-03-25 11:14:23 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -7,22 +7,20 @@
 #include <chrono>
 #include <thread>
 
-using namespace std::chrono_literals;
-
 class MockTimerClient : public TimerClient {
-public:
+ public:
   MOCK_METHOD(void, Timeout, (), (override));
 };
 
 class MockDoor : public Door {
-public:
+ public:
   MOCK_METHOD(void, lock, (), (override));
   MOCK_METHOD(void, unlock, (), (override));
   MOCK_METHOD(bool, isDoorOpened, (), (override));
 };
 
 class TimedDoorTest : public ::testing::Test {
-protected:
+ protected:
   void SetUp() override {
     door = new TimedDoor(1);
     adapter = new DoorTimerAdapter(*door);
@@ -60,14 +58,13 @@ TEST_F(TimedDoorTest, TestCallTimeout) {
   Timer timer;
   EXPECT_CALL(mockClient, Timeout()).Times(1);
   timer.tregister(1, &mockClient);
-  std::this_thread::sleep_for(2s);
+  std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 }
 
 TEST_F(TimedDoorTest, TestExceptionOpenedDoor) {
-
   door->unlock();
   ASSERT_ANY_THROW({
-    std::this_thread::sleep_for(1.5s);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1500));
     door->lock();
   });
 }
@@ -82,7 +79,7 @@ TEST_F(TimedDoorTest, TestUnlockCallsTimer) {
   Timer timer;
   EXPECT_CALL(mockClient, Timeout()).Times(1);
   timer.tregister(1, &mockClient);
-  std::this_thread::sleep_for(2s);
+  std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 }
 
 TEST_F(TimedDoorTest, TestMockDoorState) {
@@ -110,8 +107,8 @@ TEST_F(TimedDoorTest, TestFullCycle) {
   door->lock();
   EXPECT_FALSE(door->isDoorOpened());
   door->unlock();
-  std::this_thread::sleep_for(0.5s);
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));
   EXPECT_NO_THROW(door->throwState());
-  std::this_thread::sleep_for(1s);
-  EXPECT_THROW(door->lock();, std::runtime_error);
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  EXPECT_THROW(door->lock(), std::runtime_error);
 }
