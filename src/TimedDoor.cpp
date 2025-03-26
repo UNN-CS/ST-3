77de572444c84651fb5f97471ca4759c5e413c43
keshaproger <125856206+keshaproger@users.noreply.github.com>
2025-03-26 19:41:00 +0300

    Update TimedDoor.cpp

--- src/TimedDoor.cpp
+++ src/TimedDoor.cpp
@@ -1,2 +1,53 @@
 // Copyright 2021 GHA Test Team
 #include "TimedDoor.h"
+#include <stdexcept>
+#include <thread>
+#include <chrono>
+
+// Реализация конструктора адаптера
+DoorTimerAdapter::DoorTimerAdapter(TimedDoor& d) : door(d) {}
+
+// Метод Timeout вызывается по истечении таймера
+void DoorTimerAdapter::Timeout() {
+    door.throwState();
+}
+
+// Конструктор TimedDoor
+TimedDoor::TimedDoor(int timeout) : iTimeout(timeout), isOpened(false) {
+    adapter = new DoorTimerAdapter(*this);
+}
+
+// Деструктор TimedDoor
+TimedDoor::~TimedDoor() {
+    delete adapter;
+}
+
+bool TimedDoor::isDoorOpened() {
+    return isOpened;
+}
+
+void TimedDoor::unlock() {
+    isOpened = true;
+    Timer timer;
+    timer.tregister(iTimeout, adapter);
+}
+
+void TimedDoor::lock() {
+    isOpened = false;
+}
+
+int TimedDoor::getTimeOut() const {
+    return iTimeout;
+}
+
+void TimedDoor::throwState() {
+    if (isOpened) {
+        throw std::runtime_error("Door left open too long!");
+    }
+}
+
+void Timer::tregister(int timeout, TimerClient* client) {
+    client = client;
+    std::this_thread::sleep_for(std::chrono::seconds(timeout));
+    client->Timeout();
+}
