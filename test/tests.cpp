30b9f63e59fe52be1ec4107d36df0a0404b0ce2e
keshaproger <125856206+keshaproger@users.noreply.github.com>
2025-03-27 01:29:09 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -88,8 +88,3 @@ TEST(TimedDoorIntegrationTest, UnlockThenLockPreventsException) {
     DoorTimerAdapter adapter(door);
     EXPECT_NO_THROW(adapter.Timeout());
 }
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
