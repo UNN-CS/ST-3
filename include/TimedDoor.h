d3ebefa2cd579c9b540da633808f142dd05c0391
KADCHDR <125730153+KADCHDR@users.noreply.github.com>
2025-04-01 20:40:30 +0300

    Update TimedDoor.h

--- include/TimedDoor.h
+++ include/TimedDoor.h
@@ -1,52 +1,24 @@
 // Copyright 2021 GHA Test Team
 
-#ifndef INCLUDE_TIMEDDOOR_H_
-#define INCLUDE_TIMEDDOOR_H_
-
-class DoorTimerAdapter;
-class Timer;
-class Door;
-class TimedDoor;
-
-class TimerClient {
- public:
-  virtual void Timeout() = 0;
-};
-
-class Door {
- public:
-  virtual void lock() = 0;
-  virtual void unlock() = 0;
-  virtual bool isDoorOpened() = 0;
-};
-
-class DoorTimerAdapter : public TimerClient {
- private:
-  TimedDoor& door;
- public:
-  explicit DoorTimerAdapter(TimedDoor&);
-  void Timeout();
-};
-
 class TimedDoor : public Door {
  private:
-  DoorTimerAdapter * adapter;
+  DoorTimerAdapter* adapter;
+  Timer* timer;
   int iTimeout;
   bool isOpened;
  public:
   explicit TimedDoor(int);
-  bool isDoorOpened();
-  void unlock();
-  void lock();
-  int  getTimeOut() const;
+  bool isDoorOpened() override;
+  void unlock() override;
+  void lock() override;
+  int getTimeOut() const;
   void throwState();
+  Timer* getTimer();
 };
 
 class Timer {
-  TimerClient *client;
-  void sleep(int);
+  TimerClient* client;
  public:
   void tregister(int, TimerClient*);
+  void trigger();
 };
-
-#endif  // INCLUDE_TIMEDDOOR_H_
