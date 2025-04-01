ab5bff58fafe0d26b2ad4cc1733fb134d04a8391
KADCHDR <125730153+KADCHDR@users.noreply.github.com>
2025-04-01 20:43:46 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,10 +4,14 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-
 using ::testing::_;
-using ::testing::AtLeast;
-using ::testing::Throw;
+using ::testing::Mock;
+
+class MockTimer : public Timer {
+ public:
+  MOCK_METHOD(void, tregister, (int, TimerClient*), (override));
+  MOCK_METHOD(void, trigger, (), (override));
+};
 
 class MockTimedDoor : public TimedDoor {
  public:
@@ -65,18 +69,52 @@ TEST(TimedDoorTest, GetTimeoutValue) {
   EXPECT_EQ(door.getTimeOut(), 10);
 }
 
-TEST(TimedDoorTest, DoubleUnlockThrowsTwice) {
-  TimedDoor door(0);
-  door.unlock();
-  EXPECT_THROW(door.throwState(), std::runtime_error);
-  door.lock();
-  door.unlock();
-  EXPECT_THROW(door.throwState(), std::runtime_error);
-}
-
 TEST(TimedDoorTest, CloseBeforeTimeout) {
   TimedDoor door(5);
   door.unlock();
   door.lock();
   EXPECT_NO_THROW(door.throwState());
+}
+
+TEST(TimedDoorTest, ZeroTimeoutImmediateTrigger) {
+  TimedDoor door(0);
+  door.unlock();
+  EXPECT_THROW(door.getTimer()->trigger(), std::runtime_error);
+}
+
+TEST(TimerTest, TriggerWithoutRegistration) {
+  RealTimer timer;
+  EXPECT_NO_THROW(timer.trigger());
+}
+
+TEST(TimedDoorTest, LockWhileTimerPending) {
+  TimedDoor door(10);
+  door.unlock();
+  door.lock();
+  EXPECT_NO_THROW(door.getTimer()->trigger());
+}
+
+TEST(TimedDoorTest, StateAfterException) {
+  TimedDoor door(1);
+  door.unlock();
+  try { door.throwState(); } catch (...) {}
+  EXPECT_TRUE(door.isDoorOpened());
+}
+
+TEST(TimedDoorTest, StressTest) {
+  for (int i = 0; i < 1000; ++i) {
+    TimedDoor door(1);
+    door.unlock();
+    door.lock();
+  }
+}
+
+TEST(DoorTimerAdapterTest, MultipleTriggers) {
+  MockTimedDoor door(0);
+  door.unlock();
+  DoorTimerAdapter adapter(door);
+  EXPECT_CALL(door, throwState()).Times(3);
+  adapter.Timeout();
+  adapter.Timeout();
+  adapter.Timeout();
 }
