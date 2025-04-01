dcfe0e5e5220d3e23369aeafd8a7b10e4b1ba3db
KorneevaEkaterina <e.korneeva1917@gmail.com>
2025-04-01 09:21:37 +0300

    fixed

--- src/TimedDoor.cpp
+++ src/TimedDoor.cpp
@@ -7,53 +7,53 @@
 DoorTimerAdapter::DoorTimerAdapter(TimedDoor& d) : door(d) {}
 
 void DoorTimerAdapter::Timeout() {
-	if (door.isDoorOpened()) {
-		door.throwState();
-	}
+    if (door.isDoorOpened()) {
+        door.throwState();
+    }
 }
 
 TimedDoor::TimedDoor(int timeout) : iTimeout(timeout), isOpened(false) {
-	adapter = new DoorTimerAdapter(*this);
+    adapter = new DoorTimerAdapter(*this);
 }
 
 bool TimedDoor::isDoorOpened() {
-	return isOpened;
+    return isOpened;
 }
 
 void TimedDoor::unlock() {
-	isOpened = true;
-	if (adapter) {
-		Timer timer;
-		timer.tregister(iTimeout, adapter);
-	}
+    isOpened = true;
+    if (adapter) {
+        Timer timer;
+        timer.tregister(iTimeout, adapter);
+    }
 }
 
 void TimedDoor::lock() {
-	isOpened = false;
+    isOpened = false;
 }
 
 int TimedDoor::getTimeOut() const {
-	return iTimeout;
+    return iTimeout;
 }
 
 void TimedDoor::throwState() {
-	throw std::runtime_error("Door opened too long");
+    throw std::runtime_error("Door opened too long");
 }
 
 void Timer::sleep(int ms) {
-	if (ms <= 0) {
-		throw std::invalid_argument("Timer duration must be positive.");
-	}
-	std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+    if (ms <= 0) {
+        throw std::invalid_argument("Timer duration must be positive.");
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
 
 void Timer::tregister(int timeout, TimerClient* client) {
-	if (!client || timeout <= 0) {
-		throw std::invalid_argument("Invalid timer parameters");
-	}
+    if (!client || timeout <= 0) {
+        throw std::invalid_argument("Invalid timer parameters");
+    }
 
-	std::thread([this, client, timeout]() {
-		std::this_thread::sleep_for(std::chrono::milliseconds(timeout));
-		client->Timeout();
-		}).detach();
+    std::thread([this, client, timeout]() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(timeout));
+        client->Timeout();
+        }).detach();
 }
