223841369f37abf2128061f6a3d16fd324dc9a72
KADCHDR <125730153+KADCHDR@users.noreply.github.com>
2025-04-01 20:43:17 +0300

    Update TimedDoor.cpp

--- src/TimedDoor.cpp
+++ src/TimedDoor.cpp
@@ -1,51 +1,59 @@
 // Copyright 2021 GHA Test Team
 
 #include "TimedDoor.h"
-#include <stdexcept>
+#include <memory>
 
 DoorTimerAdapter::DoorTimerAdapter(TimedDoor& door) : door(door) {}
 
 void DoorTimerAdapter::Timeout() {
-    door.throwState();
+  door.throwState();
 }
 
-TimedDoor::TimedDoor(int timeout) : iTimeout(timeout), isOpened(false), timer(new Timer()) {
-    adapter = new DoorTimerAdapter(*this);
+class RealTimer : public Timer {
+ private:
+  TimerClient* client = nullptr;
+ public:
+  void tregister(int, TimerClient* client) override {
+    this->client = client;
+  }
+  void trigger() override {
+    if (client) client->Timeout();
+  }
+};
+
+TimedDoor::TimedDoor(int timeout) 
+  : iTimeout(timeout), isOpened(false), timer(new RealTimer()) {
+  adapter = new DoorTimerAdapter(*this);
+}
+
+TimedDoor::~TimedDoor() {
+  delete adapter;
+  delete timer;
 }
 
 bool TimedDoor::isDoorOpened() {
-    return isOpened;
+  return isOpened;
 }
 
 void TimedDoor::unlock() {
-    isOpened = true;
-    timer->tregister(iTimeout, adapter);
+  isOpened = true;
+  timer->tregister(iTimeout, adapter);
 }
 
 void TimedDoor::lock() {
-    isOpened = false;
+  isOpened = false;
 }
 
 int TimedDoor::getTimeOut() const {
-    return iTimeout;
+  return iTimeout;
 }
 
 void TimedDoor::throwState() {
-    if (isOpened) {
-        throw std::runtime_error("Door is still open!");
-    }
+  if (isOpened) {
+    throw std::runtime_error("Door is still open!");
+  }
 }
 
 Timer* TimedDoor::getTimer() {
-    return timer;
-}
-
-void Timer::tregister(int timeout, TimerClient* client) {
-    this->client = client;
-}
-
-void Timer::trigger() {
-    if (client) {
-        client->Timeout();
-    }
+  return timer;
 }
