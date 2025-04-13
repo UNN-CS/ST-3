ff6052ec9582a5f98db50a78af1e81e03ce85a46
Sergey Vasilev <112872559+venn2713@users.noreply.github.com>
2025-04-14 00:01:54 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,3 @@
-// Copyright 2021 GHA Test Team
 // Copyright 2025 Vasilev Sergey
 
 #include <gtest/gtest.h>
@@ -28,17 +27,43 @@ class vasilev_s_timed_door_test : public ::testing::Test {
     MockTimerClient* timerClient{};
 
  public:
-    vasilev_s_timed_door_test() : door(1), timer() {}
+    vasilev_s_timed_door_test() : door(5), timer() {}
 };
 
-TEST_F(vasilev_s_timed_door_test, Test_Initial_State) {
+TEST_F(vasilev_s_timed_door_test, Test_DoorTimerAdapter_Construction) {
+    EXPECT_NO_THROW(DoorTimerAdapter adapter(door));
+}
+
+TEST_F(vasilev_s_timed_door_test, Test_Timer_RegisterWithNullClient) {
+    EXPECT_THROW(timer.tregister(5, nullptr), std::invalid_argument);
+}
+
+TEST_F(vasilev_s_timed_door_test, Test_Door_AfterCreation_IsClosed) {
     EXPECT_FALSE(door.isDoorOpened());
-    EXPECT_EQ(door.getTimeOut(), 1);
 }
 
 TEST_F(vasilev_s_timed_door_test, Test_Door_AfterUnlock_IsOpened) {
     door.unlock();
     EXPECT_TRUE(door.isDoorOpened());
+}
+
+TEST_F(vasilev_s_timed_door_test, Test_Door_LockClosedDoor_ThrowsException) {
+    EXPECT_ANY_THROW(door.lock());
+}
+
+TEST_F(vasilev_s_timed_door_test, Test_Door_UnlockOpenedDoor_ThrowsException) {
+    door.unlock();
+    EXPECT_ANY_THROW(door.unlock());
+}
+
+TEST_F(vasilev_s_timed_door_test, Test_Door_LockAfterTimerRegister) {
+    timer.tregister(door.getTimeOut(), timerClient);
+    EXPECT_ANY_THROW(door.lock());
+}
+
+TEST_F(vasilev_s_timed_door_test, Test_Door_LockAfterUnlock_DoesNotThrow) {
+    door.unlock();
+    EXPECT_NO_THROW(door.lock());
 }
 
 TEST_F(vasilev_s_timed_door_test, Test_Door_StateAfterUnlockAndLock) {
@@ -48,77 +73,24 @@ TEST_F(vasilev_s_timed_door_test, Test_Door_StateAfterUnlockAndLock) {
     EXPECT_FALSE(door.isDoorOpened());
 }
 
-TEST_F(vasilev_s_timed_door_test, Test_Door_UnlockOpenedDoor_ThrowsException) {
+TEST_F(vasilev_s_timed_door_test, Test_Door_LockBeforeTimeout) {
     door.unlock();
-    EXPECT_THROW(door.unlock(), std::logic_error);
-}
-
-TEST_F(vasilev_s_timed_door_test, Test_Door_LockClosedDoor_ThrowsException) {
-    EXPECT_THROW(door.lock(), std::logic_error);
-}
-
-TEST_F(vasilev_s_timed_door_test, Test_Door_ThrowState) {
-    EXPECT_THROW(door.throwState(), std::runtime_error);
-    door.unlock();
-    EXPECT_THROW(door.throwState(), std::runtime_error);
-}
-
-class vasilev_s_adapter_test : public ::testing::Test {
- protected:
-    TimedDoor door;
-    DoorTimerAdapter adapter;
-
- public:
-    vasilev_s_adapter_test() : door(1), adapter(door) {}
-};
-
-TEST_F(vasilev_s_adapter_test, Test_Timeout_WhenDoorOpen) {
-    door.unlock();
-    EXPECT_THROW(adapter.Timeout(), std::runtime_error);
-}
-
-TEST_F(vasilev_s_adapter_test, Test_Timeout_WhenDoorClosed) {
-    EXPECT_NO_THROW(adapter.Timeout());
-}
-
-class vasilev_s_timer_test : public ::testing::Test {
- protected:
-    MockTimerClient client;
-    Timer timer;
-};
-
-TEST_F(vasilev_s_timer_test, Test_Timer_RegisterClient) {
-    EXPECT_CALL(client, Timeout()).Times(1);
-    timer.sleep = [](int) {};
-    timer.tregister(1, &client);
-}
-
-TEST_F(vasilev_s_timer_test, Test_Timer_RegisterWithInvalidParameters) {
-    EXPECT_THROW(timer.tregister(-1, nullptr), std::invalid_argument);
-}
-
-class TimerForTest : public Timer {
- public:
-    void sleep(int) override {
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    }
-};
-
-class vasilev_s_integration_test : public ::testing::Test {
- protected:
-    TimedDoor door;
-    DoorTimerAdapter adapter;
-    TimerForTest timer;
-
- public:
-    vasilev_s_integration_test() : door(2), adapter(door) {}
-};
-
-TEST_F(vasilev_s_integration_test, Test_DoorTimerIntegration) {
-    EXPECT_FALSE(door.isDoorOpened());
-    door.unlock();
-    EXPECT_TRUE(door.isDoorOpened());
-    EXPECT_THROW(timer.tregister(2, &adapter), std::runtime_error);
+    timer.tregister(door.getTimeOut(), timerClient);
+    std::this_thread::sleep_for(std::chrono::seconds(3));
     door.lock();
-    EXPECT_NO_THROW(timer.tregister(2, &adapter));
+    EXPECT_FALSE(door.isDoorOpened());
+}
+
+TEST_F(vasilev_s_timed_door_test, Test_Door_ThrowStateAfterTimeout) {
+    door.unlock();
+    timer.tregister(door.getTimeOut(), timerClient);
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    EXPECT_ANY_THROW(door.throwState());
+}
+
+TEST_F(vasilev_s_timed_door_test, Test_Door_LockBeforeTimeout_T) {
+    door.unlock();
+    timer.tregister(door.getTimeOut(), timerClient);
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    EXPECT_NO_THROW(door.lock());
 }
